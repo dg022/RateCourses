286dd1008ee1b65d957d16dc728a3744e68d0c55
dg022 <david.george.2480@gmail.com>
2020-07-12 14:41:22 -0600

    adding the server

---
+++ Server.js
@@ -0,0 +1,14 @@
+
+// This server file will be used during production and development builds, its takes  the build folder created after NPM run build and sends this to index.html.
+
+const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path');
+const app = express();
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
