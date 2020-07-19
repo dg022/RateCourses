1a22360b87dc197f09b31df8c278a0e46528e2cc
dg022 <dgeorg6@uwo.ca>
2020-07-19 15:49:42 -0600

    resolved deeply nested call backs for form comps

--- client/src/Components/ReviewAdd.js
+++ client/src/Components/ReviewAdd.js
@@ -2,15 +2,16 @@ import React from "react";
 import { Button, Header, Image, Modal } from "semantic-ui-react";
 import Form from "./Form.js"
 
-const ModalModalExample = () => (
+const ModalModalExample = (props) => (
   <Modal trigger={<Button>Add a review!</Button>}>
 
     <Modal.Header>  <center> Review Form   </center></Modal.Header>
     <Modal.Content image>
       <Modal.Description>
 
+ 
 
-    <Form/>
+    <Form  TextBook={props.TextBook} Difficulty={props.Difficulty} TakeAgain={props.TakeAgain}   About={props.About}/>
 
       </Modal.Description>
     </Modal.Content>
