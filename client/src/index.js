c58a131b42c1695e732e5b50042cea29a8a21ef1
nvte6 <nathanieljap@gmail.com>
2020-07-23 15:48:24 -0400

    linked pages

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,22 @@
 
 import React  from 'react'; 
 import ReactDOM from 'react-dom'; 
+
 import App from './App'; 
+import Feedback from './Feedback';
+
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+const rootElement = document.getElementById("root");
+
 ReactDOM.render(
-
-<App/>, document.querySelector('#root')
-
+	 <BrowserRouter>
+       <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/page2" component={Feedback} />
+      </Switch>
+      </BrowserRouter>,
+      rootElement
 );
 
 
