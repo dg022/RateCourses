ecb5f77369959f7171ab0eab2d4ca6c3ee788a06
dg022 <dgeorg6@uwo.ca>
2020-07-25 16:07:58 -0600

    on hover edit symbol added

---
+++ client/src/Components/Edit.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+
+const Edit  = (props) => {
+
+
+
+
+if(props.on == true){
+
+
+    return(
+
+        <i class="pencil alternate icon large "></i>
+    );
+
+
+}
+
+return(
+
+    <i class=" large pen square  icon large "></i>
+
+);
+
+
+
+
+}
+
+export default Edit; 
--- client/src/Components/SingleReview.js
+++ client/src/Components/SingleReview.js
@@ -4,7 +4,8 @@ import {  Item } from 'semantic-ui-react'
 import Avatar from 'react-avatar';
 import './Review.css';
 import ThumbsUp from './ThumbsUp.js';
- 
+import Edit from './Edit';
+
 
 
 
@@ -16,77 +17,97 @@ function toNumberString(num) {
     return num.toString(); 
   }
 }
-  
-     
+
+
+
                
 
-const ItemExampleItems = (props) => {
+class ItemExampleItems extends React.Component{
+
+  state = {
+
+    ron:false
+
+  }
+
+  render(){
+   const handleHover = () => {
+    this.setState({ron:true})
+  }
+
+  const handleLeave = () => {
+    this.setState({ron:false})
+  }
 
   var reviewID = "";
 
-  if(props.list.difficulty>=1 &&  props.list.difficulty<= 2.5 ){
+  if(this.props.list.difficulty>=1 &&  this.props.list.difficulty<= 2.5 ){
 
-    reviewID ="A"
-
-
-  }
-
-  if(props.list.difficulty>=2.5 &&  props.list.difficulty<= 3.5 ){
-
-    reviewID ="A1"
+    this.reviewID ="A"
 
 
   }
 
+  if(this.props.list.difficulty>=2.5 &&  this.props.list.difficulty<= 3.5 ){
 
-  if(props.list.difficulty>=4.0 ){
-
-    reviewID ="A2"
+    this.reviewID ="A1"
 
 
   }
 
 
-if(props.list.difficulty!=null){
-  console.log(props.list)
+  if(this.props.list.difficulty>=4.0 ){
+
+    this.reviewID ="A2"
+
+
+  }
+
+
+if(this.props.list.difficulty!=null){
+  console.log(this.props.list)
   return(
 
       <div id="ir" class="item">
       
             <div class="image">
 
-            <div class=" sb-avatar sb-avatar--icon" id={reviewID}>
+            <div class=" sb-avatar sb-avatar--icon" id={this.reviewID}>
               <div id="B" class=" sb-avatar__text">
-              <span id="C"><span>{toNumberString(props.list.difficulty)}</span></span></div></div>
+              <span id="C"><span>{toNumberString(this.props.list.difficulty)}</span></span></div></div>
             </div>
             <div class="content">
 
               <div class="meta">
-                <span>DifficultyS:{toNumberString(props.list.difficulty)}</span>
+                <span>DifficultyS:{toNumberString(this.props.list.difficulty)}</span>
               </div>
               <div class="meta"> 
-                <span>Would Take Again:{props.list.takeAgain}</span>
+                <span>Would Take Again:{this.props.list.takeAgain}</span>
               </div>
               <div id ="it" class="description">
                 <p>
-                {props.list.body} 
+                {this.props.list.body} 
                 </p>
               </div>
              
-              <ThumbsUp  id = {props.list._id}  decrementDB={props.decrementDB} incrementDB={props.incrementDB}  ThumbsUp={props.list.thumbsUp}  ThumbsDown={props.list.thumbsDown}   />
+              <ThumbsUp  id = {this.props.list._id}  decrementDB={this.props.decrementDB} incrementDB={this.props.incrementDB}  ThumbsUp={this.props.list.thumbsUp}  ThumbsDown={this.props.list.thumbsDown}   />
             </div>
 
 
-  
+            <div onMouseLeave={handleLeave} onMouseEnter={handleHover}>
+        
+            <Edit   on={this.state.ron} />
 
-    
+
+            </div>
     </div>
+
 
   );
 }
-if(props.list.difficulty==null){
+if(this.props.list.difficulty==null){
 
-  console.log(props.list)
+  console.log(this.props.list)
 
 }
 
@@ -95,7 +116,7 @@ return(<div></div>)
 
 
 }
-
+}
 
 
 
