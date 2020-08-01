07a5eb7a98679d79f9b6594420ae6e605450729e
dg022 <dgeorg6@uwo.ca>
2020-08-01 13:21:02 -0600

    fixing email input box to correct size

---
+++ client/src/Components/EInput.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Icon, Input } from 'semantic-ui-react'
+
+const InputExampleIconChild = () => (
+  <div>
+<br/> 
+    <Input iconPosition='left' placeholder='Email'>
+      <Icon name='at' />
+      <input />
+    </Input>
+  </div>
+)
+
+export default InputExampleIconChild
