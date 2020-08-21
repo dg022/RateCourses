1f6e6c64127da2febc1c7e55dc6fe56a79b488ef
dg022 <dgeorg6@uwo.ca>
2020-08-21 17:19:14 -0600

    adding build

---
+++ build/precache-manifest.56117dc416df988da5695430e6aedbe6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c1359ab06fda5b1f07a2943016d9258",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44bcbaca67b7749da1b0",
+    "url": "/static/css/main.cbcdbe4e.chunk.css"
+  },
+  {
+    "revision": "cf2c9051c99b59df593a",
+    "url": "/static/js/2.bf527ef7.chunk.js"
+  },
+  {
+    "revision": "a515304657dd8a663548f9dde8012f85",
+    "url": "/static/js/2.bf527ef7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44bcbaca67b7749da1b0",
+    "url": "/static/js/main.966a5624.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
