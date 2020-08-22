17f2afd6267085a01657f58999a28d1cf747aeee
dg022 <dgeorg6@uwo.ca>
2020-08-21 18:23:59 -0600

    fixing server

--- Server.js
+++ Server.js
@@ -14,7 +14,7 @@ mongoose.connect(cons, { useNewUrlParser: true });
 var Codes = mongoose.model('Codes', Posts.model);
 
 
-//app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
